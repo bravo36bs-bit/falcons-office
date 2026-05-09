3e14476b08b610981871ddbf664115b550e1fb28
bravo36bs-bit <bravo36bs@gmail.com>
2026-05-09 09:05:41 -0700

    Fix Aiven connection

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 10000,
     ssl: {
     rejectUnauthorized: false
   }
