984ad643e727cbd2c8a65350b32321e4643fc4ef
bravo36bs-bit <bravo36bs@gmail.com>
2026-05-09 08:53:14 -0700

    Add SSL for Aiven

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(limiter);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
     ssl: {
     rejectUnauthorized: false
